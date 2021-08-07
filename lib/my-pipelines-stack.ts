cbb2e5ff2759f669974dbd4dddb25bbfcd5bc91a
86331181+dppuligu <86331181+dppuligu@users.noreply.github.com>
2021-08-07 15:37:37 +0000

    Adjusted code

--- lib/my-pipelines-stack.ts
+++ lib/my-pipelines-stack.ts
@@ -1,38 +1,37 @@
 import { Stack, StackProps, Construct, SecretValue } from '@aws-cdk/core';
 import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
-
+import { PolicyStatement, Effect } from '@aws-cdk/aws-iam'
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
-
 export class MyPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
     const sourceArtifact = new codepipeline.Artifact();
     const cloudAssemblyArtifact = new codepipeline.Artifact();
-
+    // const secretManagerAccessPolicy = new PolicyStatement({
+    //     effect: Effect.ALLOW,
+    //     actions: ["secretsmanager:GetSecretValue"],
+    //     resources: ["*"]
+    //   })    
     const pipeline = new CdkPipeline(this, 'Pipeline', {
       pipelineName: 'MyAppPipeline',
       cloudAssemblyArtifact,
-
       sourceAction: new codepipeline_actions.GitHubSourceAction({
         actionName: 'GitHub',
         output: sourceArtifact,
-        oauthToken: SecretValue.secretsManager('githup_p_token'),
+        oauthToken: SecretValue.secretsManager('token_cdkpipelines_repo', {jsonField:'token_for_cdk-pipelines_repo'}),
         trigger: codepipeline_actions.GitHubTrigger.POLL,
-        // Replace these with your actual GitHub project info
         owner: 'dppuligu',
-        repo: 'https://github.com/dppuligu/examples.git',
-        branch: 'main'
+        repo: 'cdk-pipelines',
+        branch: 'master'
       }),
-
       synthAction: SimpleSynthAction.standardNpmSynth({
         sourceArtifact,
         cloudAssemblyArtifact,
-
-        // Use this if you need a build step (if you're not using ts-node
-        // or if you have TypeScript Lambdas that need to be compiled).
+        installCommand: 'npm install',
         buildCommand: 'npm run build',
+        synthCommand: 'npm ci && npx cdk synth',
+        // rolePolicyStatements: [secretManagerAccessPolicy]
       }),
     });
   }
